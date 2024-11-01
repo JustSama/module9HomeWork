69433fb3d842b7bda9ce39326700d5d3c48252bc
JustSama <106009367+JustSama@users.noreply.github.com>
2024-11-02 00:36:36 +0500

    Add files via upload

---
+++ HomeWork/adapter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+interface IPaymentProcessor
+{
+    void ProcessPayment(double amount);
+}
+
+class PayPalPaymentProcessor : IPaymentProcessor
+{
+    public void ProcessPayment(double amount)
+    {
+        Console.WriteLine($"Обработка платежей {amount} через PayPal.");
+    }
+}
+
+class StripePaymentService
+{
+    public void MakeTransaction(double totalAmount)
+    {
+        Console.WriteLine($"Обработка платежей {totalAmount} через PayPal.");
+    }
+}
+
+class StripePaymentAdapter : IPaymentProcessor
+{
+    private readonly StripePaymentService stripePaymentService;
+
+    public StripePaymentAdapter(StripePaymentService stripePaymentService)
+    {
+        this.stripePaymentService = stripePaymentService;
+    }
+
+    public void ProcessPayment(double amount)
+    {
+        stripePaymentService.MakeTransaction(amount);
+    }
+}
+
+class SquarePaymentService
+{
+    public void ExecutePayment(double paymentAmount)
+    {
+        Console.WriteLine($"Processing payment of {paymentAmount} through Square.");
+    }
+}
+
+class SquarePaymentAdapter : IPaymentProcessor
+{
+    private readonly SquarePaymentService squarePaymentService;
+
+    public SquarePaymentAdapter(SquarePaymentService squarePaymentService)
+    {
+        this.squarePaymentService = squarePaymentService;
+    }
+
+    public void ProcessPayment(double amount)
+    {
+        squarePaymentService.ExecutePayment(amount);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IPaymentProcessor paypalProcessor = new PayPalPaymentProcessor();
+        paypalProcessor.ProcessPayment(100.0);
+
+        IPaymentProcessor stripeProcessor = new StripePaymentAdapter(new StripePaymentService());
+        stripeProcessor.ProcessPayment(200.0);
+
+        IPaymentProcessor squareProcessor = new SquarePaymentAdapter(new SquarePaymentService());
+        squareProcessor.ProcessPayment(300.0);
+    }
+}
---
+++ HomeWork/decor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+interface Beverage
+{
+    string Description { get; }
+    double Cost();
+}
+
+class Espresso : Beverage
+{
+    public string Description => "Espresso";
+    public double Cost() => 50.0;
+}
+
+class Tea : Beverage
+{
+    public string Description => "Tea";
+    public double Cost() => 30.0;
+}
+
+class Latte : Beverage
+{
+    public string Description => "Latte";
+    public double Cost() => 70.0;
+}
+
+class Mocha : Beverage
+{
+    public string Description => "Mocha";
+    public double Cost() => 80.0;
+}
+
+abstract class BeverageDecorator : Beverage
+{
+    protected Beverage beverage;
+    public BeverageDecorator(Beverage beverage) { this.beverage = beverage; }
+    public virtual string Description => beverage.Description;
+    public abstract double Cost();
+}
+
+class Milk : BeverageDecorator
+{
+    public Milk(Beverage beverage) : base(beverage) { }
+    public override string Description => beverage.Description + ", Milk";
+    public override double Cost() => beverage.Cost() + 10.0;
+}
+
+class Sugar : BeverageDecorator
+{
+    public Sugar(Beverage beverage) : base(beverage) { }
+    public override string Description => beverage.Description + ", Sugar";
+    public override double Cost() => beverage.Cost() + 5.0;
+}
+
+class WhippedCream : BeverageDecorator
+{
+    public WhippedCream(Beverage beverage) : base(beverage) { }
+    public override string Description => beverage.Description + ", Whipped Cream";
+    public override double Cost() => beverage.Cost() + 15.0;
+}
+
+class Syrup : BeverageDecorator
+{
+    public Syrup(Beverage beverage) : base(beverage) { }
+    public override string Description => beverage.Description + ", Syrup";
+    public override double Cost() => beverage.Cost() + 20.0;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Beverage beverage = new Espresso();
+        beverage = new Milk(beverage);
+        beverage = new Sugar(beverage);
+        beverage = new WhippedCream(beverage);
+        Console.WriteLine($"{beverage.Description}: {beverage.Cost()}");
+
+        Beverage beverage2 = new Latte();
+        beverage2 = new Syrup(beverage2);
+        beverage2 = new Milk(beverage2);
+        Console.WriteLine($"{beverage2.Description}: {beverage2.Cost()}");
+
+        Beverage beverage3 = new Mocha();
+        beverage3 = new WhippedCream(beverage3);
+        beverage3 = new Sugar(beverage3);
+        beverage3 = new Syrup(beverage3);
+        Console.WriteLine($"{beverage3.Description}: {beverage3.Cost()}");
+    }
+}
